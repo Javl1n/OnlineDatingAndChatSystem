cd0846618f12cf8419445996a537fc85667954d4
Javl1n <farmodia@gmail.com>
2023-11-11 18:33:59 +0800

    Chat View Done

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -25,9 +25,27 @@
             </div> --}}
             <livewire:search-users />
         </div>
-        <div class="flex flex-col w-4/6 flex-shrink-0 text-slate-800 dark:text-white overflow-y-auto lg:h-[10vh] xl:h-[85vh]">
-            <div class="flex-1">
-                sa
+        <div class="flex flex-col w-4/6 flex-shrink-0 text-slate-800 dark:text-white overflow-y-auto lg:h-[10vh] xl:h-[85vh] border">
+            <div class="h-24 flex bg-white p-5">
+                <img src="https://i.pravatar.cc/60" width="60" height="60" alt="" class="mr-4 rounded-full">
+                <div class="flex flex-col justify-center">
+                    <span class="font-bold m-0">ME na too</span>
+                    <small class="text-gray-500 m-0">online</small>
+                </div>
+            </div>
+            <div class="flex-1 flex flex-col my-4 mx-5">
+                <div class="grid mb-4">
+                    <div class="justify-self-start flex">
+                        <div class="grid"><img src="https://i.pravatar.cc/50" width="50" height="50" alt="" class="mr-4 rounded-full align-end"></div>
+                            <div class=" rounded-xl bg-white pt-3 px-5">sladjlasjk</div>
+                    </div>
+                </div>
+                <div class="grid mb-4">
+                    <div class="justify-self-end flex">
+                        <div class=" rounded-xl bg-white pt-3 px-5">sladjlasjk</div>
+                        <div class="grid"><img src="https://i.pravatar.cc/50" width="50" height="50" alt="" class="ml-4 rounded-full align-end"></div>
+                    </div>
+                </div>
             </div>
             <div class="bg-white shadow dark:bg-transparent dark:shadow-indigo-400 p-4 flex content-end">
                 <x-icon name="plus-fill" height="30" width="30" class="mr-2 my-auto fill-indigo-400" />
