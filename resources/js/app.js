17411c690a6e1875b1587745321ec420e9873ea5
Javl1n <farmodia@gmail.com>
2023-11-11 23:05:49 +0800

    Events Added, Broadcast Installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// window.Pusher = Pusher;
+ 
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//     forceTLS: true
+// });
